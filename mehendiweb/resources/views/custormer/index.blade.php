e7d422581c31f74f19656acfd69851b88ed5b720
Kavishka Chamudith <kavishkachamuditha95@gmail.com>
2023-03-22 23:35:18 +0530

    cards

--- mehendiweb/resources/views/custormer/index.blade.php
+++ mehendiweb/resources/views/custormer/index.blade.php
@@ -25,93 +25,147 @@
      <P class="text-light text-margin">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br> Assumenda cumque impedit sapiente quaerat repudiandae, nconsequatur. <br> Deleniti mollitia nemo quas repudiandae.</P>
      <div class="btn bannebtn-margin">Click Here</div>
 </div>
-
- <div class="container">
-    <div class="row">
-        {{-- <h1 class="text-light">READY TO ORDER A COFFEE</h1> --}}
-       
-          <div class="container noselect" style="margin-top:300px;">
-            <div class="canvas">
-              <div class="tracker tr-1"></div>
-              <div class="tracker tr-2"></div>
-              <div class="tracker tr-3"></div>
-              <div class="tracker tr-4"></div>
-              <div class="tracker tr-5"></div>
-              <div class="tracker tr-6"></div>
-              <div class="tracker tr-7"></div>
-              <div class="tracker tr-8"></div>
-              <div class="tracker tr-9"></div>
-              <div class="tracker tr-10"></div>
-              <div class="tracker tr-11"></div>
-              <div class="tracker tr-12"></div>
-              <div class="tracker tr-13"></div>
-              <div class="tracker tr-14"></div>
-              <div class="tracker tr-15"></div>
-              <div class="tracker tr-16"></div>
-              <div class="tracker tr-17"></div>
-              <div class="tracker tr-18"></div>
-              <div class="tracker tr-19"></div>
-              <div class="tracker tr-20"></div>
-              <div class="tracker tr-21"></div>
-              <div class="tracker tr-22"></div>
-              <div class="tracker tr-23"></div>
-              <div class="tracker tr-24"></div>
-              <div class="tracker tr-25"></div>
-              <div id="card">
-                <img src="{{ URL('images/Untitled design.png') }}" alt="" style="width:300px;">
-              <p id="prompt">Hello Guys Let's Order a Sri Lankan Finast Coffee</p>
-                <div class="title btn btn-midddle">Order Now</div>            
-              </div>
-            </div>
-          </div>
-        </div>
-    </div>
-
   
           <div class="col-md-12 margincard">
             <h1 class="textclr text-center txtwhat">What we Offer</h1>
-            <div class="card-deck">.
+            <div class="card-deck">
               <div class="card cardbackgrnd">
                 <img class="card-img-top" src="{{ URL('images/card (1).jpg') }}" alt="Card image cap">
                 <div class="card-body">
-                  <h5 class="card-title text-center">Drink a Coffee</h5>
-                  <p class="card-text">This is the text for Card 1.</p>
+                  <h5 class="card-title text-center fontsize">Drink a Coffee</h5>
                 </div>
               </div>
     
               <div class="card cardbackgrnd">
                 <img class="card-img-top" src="{{ URL('images/card (2).jpg') }}" alt="Card image cap">
                 <div class="card-body">
-                  <h5 class="card-title text-center">Order a Coffee</h5>
-                  <p class="card-text">This is the text for Card 2.</p>
+                  <h5 class="card-title text-center fontsize">Order a Coffee</h5>
                 </div>
               </div>
     
               <div class="card cardbackgrnd">
                 <img class="card-img-top" src="{{ URL('images/card (3).jpg') }}" alt="Card image cap">
                 <div class="card-body">
-                  <h5 class="card-title text-center">Eat & Drink</h5>
-                  <p class="card-text">This is the text for Card 3.</p>
+                  <h5 class="card-title text-center fontsize">Eat & Drink</h5>
                 </div>
               </div>
     
               <div class="card cardbackgrnd">
                 <img class="card-img-top" src="{{ URL('images/card (4).jpg') }}" alt="Card image cap">
                 <div class="card-body">
-                  <h5 class="card-title text-center">Don't Drink</h5>
-                  <p class="card-text">This is the text for Card 4.</p>
+                  <h5 class="card-title text-center fontsize">Don't Drink</h5>
                 </div>
               </div>
     
               <div class="card cardbackgrnd">
                 <img class="card-img-top" src="{{ URL('images/card (5).jpg') }}" alt="Card image cap">
                 <div class="card-body">
-                  <h5 class="card-title text-center">Drink One Coffee</h5>
-                  <p class="card-text">This is the text for Card 5.</p>
+                  <h5 class="card-title text-center fontsize">Drink One Coffee</h5>
               </div>
 
           </div>
         </div>
       </div>
  
-      <h1 class="textclr text-center txtwhat">What we Offer</h1>
+      <h1 class="textclr text-center txtwhat">Our Porducts</h1>
+
+
+            <div class="row">
+        
+            <div class="col-lg-3 col-md-6">
+            <div class="card card-clr" style="max-width:20rem;">
+              <button class="btn btn-danger btncart">
+                <i class="fa fa-shopping-cart"  style="font-size: 30px;"></i>
+              </button>
+              <button class="btn btn-danger btnwishlist">
+                <i class="fa-solid fa-heart" style="font-size: 30px;"></i>
+              </button>
+              <img src="{{ URL('images/cardimg.png') }}" alt="" style="width:300px;">
+              <div class="card-body">
+                <button class="btn btn-danger btnbuy">Buy Now</button>
+                <h5 class="card-title text-center fontsize">Drink One Coffee</h5>
+            </div>
+            </div>
+          </div>
+
+        <div class="col-lg-3 col-md-6">           
+            <div class="card card-clr" style="max-width:20rem;">
+              <button class="btn btn-danger btncart">
+                <i class="fa fa-shopping-cart"  style="font-size: 30px;"></i>
+              </button>
+              <button class="btn btn-danger btnwishlist">
+                <i class="fa-solid fa-heart" style="font-size: 30px;"></i>
+              </button>
+              <img src="{{ URL('images/cardimg.png') }}" alt="" style="width:300px;">
+              <div class="card-body">
+                <button class="btn btn-danger btnbuy">Buy Now</button>
+                <h5 class="card-title text-center fontsize">Drink One Coffee</h5>
+            </div>
+            </div>
+            </div> 
+
+            <div class="col-lg-3 col-md-6">           
+               <div class="card card-clr" style="max-width:20rem;">
+              <button class="btn btn-danger btncart">
+                <i class="fa fa-shopping-cart"  style="font-size: 30px;"></i>
+              </button>
+              <button class="btn btn-danger btnwishlist">
+                <i class="fa-solid fa-heart" style="font-size: 30px;"></i>
+              </button>
+              <img src="{{ URL('images/cardimg.png') }}" alt="" style="width:300px;">
+              <div class="card-body">
+                <button class="btn btn-danger btnbuy">Buy Now</button>
+                <h5 class="card-title text-center fontsize">Drink One Coffee</h5>
+            </div>
+            </div>
+            </div>
+
+            <div class="col-lg-3 col-md-6">           
+              <div class="card card-clr" style="max-width:20rem;">
+             <button class="btn btn-danger btncart">
+               <i class="fa fa-shopping-cart"  style="font-size: 30px;"></i>
+             </button>
+             <button class="btn btn-danger btnwishlist">
+               <i class="fa-solid fa-heart" style="font-size: 30px;"></i>
+             </button>
+             <img src="{{ URL('images/cardimg.png') }}" alt="" style="width:300px;">
+             <div class="card-body">
+               <button class="btn btn-danger btnbuy">Buy Now</button>
+               <h5 class="card-title text-center fontsize">Drink One Coffee</h5>
+           </div>
+           </div>
+           </div>
+        </div>
+      </div>
+
+ 
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
+<br>
