fab4ab769d2031d4679b8029b432c105fa499145
Kavishka Chamudith <kavishkachamuditha95@gmail.com>
2023-03-23 10:33:24 +0530

    furniture

--- mehendiweb/resources/views/custormer/index.blade.php
+++ mehendiweb/resources/views/custormer/index.blade.php
@@ -1,165 +1,33 @@
 @extends('custormer.layout')
 
-{{-- nav bar code start from here --}}
-<nav>  
-    <img class="logo" src="{{URL('images/Untitleddesig.png') }}" alt="">
-    </div>
-    <ul class="nav-links">
-      <li><a href="#">Home</a></li>
-      <li><a href="#">About</a></li>
-      <li><a href="#">Services</a></li>
-      <li><a href="#">Contact</a></li>
-    </ul>
-    <div class="burger">
-      <div class="line"></div>
-      <div class="line"></div>
-      <div class="line"></div>
-    </div>
-  </nav>
-{{-- nav bar code stop from here --}}
 
 <div class="backgroundclr">
-    <img class="img-two-class" src="{{ URL('images/pngwing.com (2).png') }}" alt="">  
-     <img class="img-class" src="{{ URL('images/pngwing.com (1).png') }}" alt="">
-     <h1 class="banne-txt">SRI LANKA'S<br> COFFEE SUPPLIES</h1>
-     <P class="text-light text-margin">Lorem ipsum dolor sit amet consectetur adipisicing elit. <br> Assumenda cumque impedit sapiente quaerat repudiandae, nconsequatur. <br> Deleniti mollitia nemo quas repudiandae.</P>
-     <div class="btn bannebtn-margin">Click Here</div>
+  <div class="nav-container">
+    <nav>  
+      <img class="logo" src="{{URL('images/Untitleddesig.png') }}" alt="">
+      <ul class="nav-links">
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+      <div class="burger">
+        <div class="line"></div>
+        <div class="line"></div>
+        <div class="line"></div>
+      </div>
+    </nav>
+    <h1 class="adrianafurniture">Arden Furniture</h1>   
+    <h3 class="imgtext">We are the Arden Furniture.</h3> 
+  </div>
+  <img class="img-two-class" src="{{ URL('images/Untitled design.jpg') }}" alt="" style="width:100%;"> 
 </div>
-  
-          <div class="col-md-12 margincard">
-            <h1 class="textclr text-center txtwhat">What we Offer</h1>
-            <div class="card-deck">
-              <div class="card cardbackgrnd">
-                <img class="card-img-top" src="{{ URL('images/card (1).jpg') }}" alt="Card image cap">
-                <div class="card-body">
-                  <h5 class="card-title text-center fontsize">Drink a Coffee</h5>
-                </div>
-              </div>
-    
-              <div class="card cardbackgrnd">
-                <img class="card-img-top" src="{{ URL('images/card (2).jpg') }}" alt="Card image cap">
-                <div class="card-body">
-                  <h5 class="card-title text-center fontsize">Order a Coffee</h5>
-                </div>
-              </div>
-    
-              <div class="card cardbackgrnd">
-                <img class="card-img-top" src="{{ URL('images/card (3).jpg') }}" alt="Card image cap">
-                <div class="card-body">
-                  <h5 class="card-title text-center fontsize">Eat & Drink</h5>
-                </div>
-              </div>
-    
-              <div class="card cardbackgrnd">
-                <img class="card-img-top" src="{{ URL('images/card (4).jpg') }}" alt="Card image cap">
-                <div class="card-body">
-                  <h5 class="card-title text-center fontsize">Don't Drink</h5>
-                </div>
-              </div>
-    
-              <div class="card cardbackgrnd">
-                <img class="card-img-top" src="{{ URL('images/card (5).jpg') }}" alt="Card image cap">
-                <div class="card-body">
-                  <h5 class="card-title text-center fontsize">Drink One Coffee</h5>
-              </div>
 
-          </div>
-        </div>
-      </div>
- 
-      <h1 class="textclr text-center txtwhat">Our Porducts</h1>
+<br>
+<br>
+<br>
+<br>
 
-
-            <div class="row">
-        
-            <div class="col-lg-3 col-md-6">
-            <div class="card card-clr" style="max-width:20rem;">
-              <button class="btn btn-danger btncart">
-                <i class="fa fa-shopping-cart"  style="font-size: 30px;"></i>
-              </button>
-              <button class="btn btn-danger btnwishlist">
-                <i class="fa-solid fa-heart" style="font-size: 30px;"></i>
-              </button>
-              <img src="{{ URL('images/cardimg.png') }}" alt="" style="width:300px;">
-              <div class="card-body">
-                <button class="btn btn-danger btnbuy">Buy Now</button>
-                <h5 class="card-title text-center fontsize">Drink One Coffee</h5>
-            </div>
-            </div>
-          </div>
-
-        <div class="col-lg-3 col-md-6">           
-            <div class="card card-clr" style="max-width:20rem;">
-              <button class="btn btn-danger btncart">
-                <i class="fa fa-shopping-cart"  style="font-size: 30px;"></i>
-              </button>
-              <button class="btn btn-danger btnwishlist">
-                <i class="fa-solid fa-heart" style="font-size: 30px;"></i>
-              </button>
-              <img src="{{ URL('images/cardimg.png') }}" alt="" style="width:300px;">
-              <div class="card-body">
-                <button class="btn btn-danger btnbuy">Buy Now</button>
-                <h5 class="card-title text-center fontsize">Drink One Coffee</h5>
-            </div>
-            </div>
-            </div> 
-
-            <div class="col-lg-3 col-md-6">           
-               <div class="card card-clr" style="max-width:20rem;">
-              <button class="btn btn-danger btncart">
-                <i class="fa fa-shopping-cart"  style="font-size: 30px;"></i>
-              </button>
-              <button class="btn btn-danger btnwishlist">
-                <i class="fa-solid fa-heart" style="font-size: 30px;"></i>
-              </button>
-              <img src="{{ URL('images/cardimg.png') }}" alt="" style="width:300px;">
-              <div class="card-body">
-                <button class="btn btn-danger btnbuy">Buy Now</button>
-                <h5 class="card-title text-center fontsize">Drink One Coffee</h5>
-            </div>
-            </div>
-            </div>
-
-            <div class="col-lg-3 col-md-6">           
-              <div class="card card-clr" style="max-width:20rem;">
-             <button class="btn btn-danger btncart">
-               <i class="fa fa-shopping-cart"  style="font-size: 30px;"></i>
-             </button>
-             <button class="btn btn-danger btnwishlist">
-               <i class="fa-solid fa-heart" style="font-size: 30px;"></i>
-             </button>
-             <img src="{{ URL('images/cardimg.png') }}" alt="" style="width:300px;">
-             <div class="card-body">
-               <button class="btn btn-danger btnbuy">Buy Now</button>
-               <h5 class="card-title text-center fontsize">Drink One Coffee</h5>
-           </div>
-           </div>
-           </div>
-        </div>
-      </div>
-
- 
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
-<br>
 <br>
 <br>
 <br>
